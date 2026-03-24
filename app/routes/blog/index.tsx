0b5ae06bfef6ad772ca806aa0fd23f36bea22dca
Brent Feuerbacher <feuerbacherb@gmail.com>
2026-03-24 13:14:45 -0500

    created a way to filter posts

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { PostMeta } from "~/types";
 import PostCard from "~/components/PostCard";
 import Pagination from "~/components/Pagination";
 import { useState } from "react";
+import PostFilter from "~/components/PostFilter";
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL("/posts-meta.json", request.url);
@@ -22,24 +23,45 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const { posts } = loaderData as { posts: PostMeta[] };
   const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 3;
+  const postsPerPage = 10;
 
-  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLowerCase();
+    return post.title.toLowerCase().includes(query) || post.excerpt.toLowerCase().includes(query);
+  });
+
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage);
   const indexOfLast = currentPage * postsPerPage;
   const indexOfFirst = indexOfLast - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
 
   return (
     <section className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
       <h2 className='text-3xl font-bold mb-8 text-white'>📝 Blog</h2>
-      {currentPosts.map((post) => (
-        <PostCard
-          key={post.id}
-          post={post}
-        />
-      ))}
+
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(query) => {
+          setSearchQuery(query);
+          setCurrentPage(1);
+        }}
+      />
+      <div className='space-y-8'>
+        {currentPosts.length === 0 ? (
+          <p className='text-gray-400 text-center'>No posts found.</p>
+        ) : (
+          currentPosts.map((post) => (
+            <PostCard
+              key={post.id}
+              post={post}
+            />
+          ))
+        )}
+      </div>
 
       {totalPages > 1 && (
         <Pagination
