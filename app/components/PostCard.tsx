b65fac88f23a05271ac5d50278eb1f9f20c0da5b
Brent Feuerbacher <feuerbacherb@gmail.com>
2026-03-26 11:12:16 -0500

    created post type and fixed routing issues for posts

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -6,6 +6,12 @@ const PostCard = ({ post }: { post: PostMeta }) => {
     <article className='bg-gray-800 p-6 rounded-lg shadow'>
       <h3 className='text-2xl font-semibold text-blue-400'>{post.title}</h3>
       <p className='text-sm text-gray-400 mb-2'>{new Date(post.date).toDateString()}</p>
+      {post.image && (
+        <img
+          src={post.image}
+          alt={post.title}
+          className='w-full h-48 object-cover rounded mb-4'></img>
+      )}
       <p className='text-gray-300 mb-4'>{post.excerpt}</p>
       <Link
         to={`/blog/${post.slug}`}
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import FeaturedProjects from "~/components/FeaturedProjects";
 import AboutPreview from "~/components/AboutPreview";
 import LatestPosts from "~/components/LatestPosts";
-import type { Project } from "~/types";
-import type { PostMeta } from "~/types";
+import type { Project, PostMeta, StrapiProject, StrapiResponse } from "~/types";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "The Friendly Dev | Welcome" }, { name: "description", content: "Custom Website Development" }];
@@ -13,15 +12,28 @@ export function meta({}: Route.MetaArgs) {
 export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
   const url = new URL(request.url);
 
-  const [projectRes, postRes] = await Promise.all([fetch(`${import.meta.env.VITE_API_URL}/projects`), fetch(new URL("/posts-meta.json", url))]);
+  const [projectRes, postRes] = await Promise.all([fetch(`${import.meta.env.VITE_API_URL}/projects?filters[featured][$eq]=true&populate=*`), fetch(new URL("/posts-meta.json", url))]);
 
   if (!projectRes.ok || !postRes.ok) {
     throw new Error("Failed to fetch projects or posts");
   }
 
-  const [projects, posts] = await Promise.all([projectRes.json(), postRes.json()]);
+  const projectJson: StrapiResponse<StrapiProject> = await projectRes.json();
+  const postJson = await postRes.json();
 
-  return { projects, posts };
+  const projects = projectJson.data.map((item) => ({
+    id: item.id,
+    documentId: item.documentId,
+    title: item.title,
+    description: item.description,
+    image: item.image?.url ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}` : "/images/no-image.png",
+    url: item.url,
+    date: item.date,
+    category: item.category,
+    featured: item.featured,
+  }));
+
+  return { projects, posts: postJson };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
